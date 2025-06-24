2788fd0993d5d361cbe92ce16e0b678d29baeb0e
김성민 <98kimsungmin@naver.com>
2025-06-25 04:59:37 +0900

    build: 정적 링크 플래그 추가 및 Windows 콘솔 UTF-8 설정
    - CMakeLists.txt에 CMAKE_EXE_LINKER_FLAGS "-static" 추가
    - main 함수에서 Windows 환경 시 SetConsoleOutputCP(CP_UTF8) 및 SetConsoleCP(CP_UTF8) 적용

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "repl/repl.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);  // 코드 페이지를 UTF-8로 설정
+    SetConsoleCP(CP_UTF8);
+#endif
+
     Repl repl;
     // repl.TestLexer();
     // repl.TestParser();
