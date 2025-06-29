ef5fd844f8f65f1fb9945388b1d34d91adf0f50d
tolelom <98kimsungmin@naver.com>
2025-06-30 08:14:43 +0900

    Feat: 프로그램의 모드 추가(구현 중) - 기존의 인터프리터 모드, 파일 모드(구현 예정), 사용법(구현 예정)의 초안 - .clang-format 파일 추가(GOOGLE STYLE)

--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
+#include <iostream>
 #include "repl/repl.h"
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-int main() {
+int main(int argc, char *argv[]) {
 #ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);  // 코드 페이지를 UTF-8로 설정
-    SetConsoleCP(CP_UTF8);
+  SetConsoleOutputCP(CP_UTF8);  // 코드 페이지를 UTF-8로 설정
+  SetConsoleCP(CP_UTF8);
 #endif
 
+  if (argc > 1) {
+    std::cout << "사용법: hongik [script]"; // 추후 추가 예정
+    return 1;
+  }
+
+  // 파일 모드
+  if (argc == 1) {
+  }
+
+  // 인터프리터 모드
+  if (argc == 0) {
     Repl repl;
     // repl.TestLexer();
     // repl.TestParser();
     repl.Run();
-    return 0;
+  }
+
+
+
+  return 0;
 }
